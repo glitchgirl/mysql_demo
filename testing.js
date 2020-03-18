936622675b1369fd6eb7c784ee9d8fcb2be46654
glitchgirl <londonspleen@gmail.com>
2020-03-18 11:48:34 -0400

    borked

--- testing.js
+++ testing.js
@@ -7,11 +7,25 @@ var connection = mysql.createConnection({
     database : 'resturant'
 });
 
+
+
 connection.connect();
 // insert into
-
+function addData(){
+    var curry = [9, 'great buffet', 'mid', '$$', 'A', 'washington rd', 'indian', 'B', 'Curry Hut', 'dine in', 'buffet', 'y', 'chill', 'vegan friendly', 'decorations', 'y'];
+    connection.query("insert into augusta (ratings, review, traffic, price, grade, location, type_of_food, service, name_rest, type_rest, amenities, delivery, atmoshpere, options_rest, cool_stuff, locally_owned) values ?",curry,function(error, results, fields){
+        if (error) throw error;
+        console.log(results);
+    });
+}
 //show info
-
+function print(){
+connection.query('SELECT * from evans AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results);
+  });
+}
 //delete info
 
+addData();
 connection.end();
